a7a06d9013322068c92b3844e3d71d10d25c3e92
Anaïs Huoch <anaisgagneur@gmail.com>
2020-03-18 13:13:28 +0100

    debugging

--- exercises/min.js
+++ exercises/min.js
@@ -11,6 +11,18 @@
 
 // Your code:
 
+
+let number1 = 30;
+let number2 = 50;
+
+function min(number1, number2) {
+    if (number1 < number2) {
+        return number1;
+    } else {
+        return number2;
+    }
+}
+
 //* Begin of tests
 const assert = require('assert');
 
